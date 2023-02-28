f84572e39b2f331e4f89c22d13f0acc83b3f375a
Paras Pidurkar <45849288+ParasPidurkar@users.noreply.github.com>
2023-02-28 23:32:41 +0530

    Add files via upload

---
+++ CWH/code/40_exercise3/script.js
@@ -0,0 +1,22 @@
+console.log("from script file");
+let arr =[
+    "How many programmers does it take to change a light bulb?None. It’s a hardware problem.",
+    "What is hardware? (According to coders) It’s the part of a computer that can kick you.",
+    "What is software? (According to developers) It’s the part of a computer you can’t hit. ",
+    "What happened to your funny programming jokes?” asks the CTO. They’re still loading,” replied the junior developer.",
+    "Eight bytes walk into a bar. The bartender asks, “Can I get you anything?” Yes, reply the bytes. Make us a double.",
+    "Why do coders always mix up Halloween and Christmas? Because Oct 31 = Dec 25 (Coding joke explained: 5×5=25 in the DECimal number system is equal to 5×5=31 in the OCTal number system.) ",
+    "Knock, knock. Who’s There? Very long pause…. Java.",
+    "Why did the programmer die in the shower? He read the shampoo bottle instructions: Lather. Rinse. Repeat. ",
+    "What does programming consist of? 10% science 20% ingenuity 70% getting the ingenuity to work with the science ",
+    "How did Yoda get his first lead? He used the Sales Force. (See more marketing jokes here.) ",
+    "Have you heard about the new Cray supercomputer? It’s so fast, that it executes an infinite loop in 6 seconds. ",
+    "What is debugging? Removing the needles from the haystack."
+    ]
+
+function randomJoke()
+{
+    return arr[Math.floor(Math.random() *12)]
+}
+
+document.write(randomJoke())
